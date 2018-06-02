be8cff9710dd10625c1c8d8b47ddaf4ca59208ad
CarlosLannister <carloslannister@gmail.com>
2018-06-02 14:45:20 +0200

    Added mock and prod versions

--- gangame.sdk/src/main/java/alphasecurity/com/gangamesdk/GangameApiService.kt
+++ gangame.sdk/src/main/java/alphasecurity/com/gangamesdk/GangameApiService.kt
@@ -1,17 +1,24 @@
 package alphasecurity.com.gangamesdk
 
+import alphasecurity.com.gangamesdk.serializer.TopGameDeserializer
+import com.alphasecurity.gangamesdk.GangameClientConfig
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class GangameApiService {
+class GangameApiService(val apiConfig: GangameApiConfig = GangameClientConfig()) {
 
     val apiClient: RetrofitGangameApi
 
     init {
+        val gson = GsonBuilder().registerTypeAdapter(TopGame::class.java, TopGameDeserializer())
+                .create()
+
         val apiClientConfig = Retrofit.Builder().baseUrl(Routes.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create()).build()
+                .addConverterFactory(GsonConverterFactory.create(gson))
 
-        apiClient = apiClientConfig.create(RetrofitGangameApi::class.java)
+        apiConfig.setupConfig(apiClientConfig)
 
+        apiClient = apiClientConfig.build().create(RetrofitGangameApi::class.java)
     }
 }
